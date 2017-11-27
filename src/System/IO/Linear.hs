f8945ff43316b42992d3895ce7ad296f7c528d63
Mathieu Boespflug <m@tweag.io>
2017-11-27 08:58:18 +0100

    Rename IO modules and monads.
    - `System.IO.Linear` contains the IO monad but with linear types.
    - `System.IO.Resource` defines the RIO monad.

--- src/System/IO/Linear.hs
+++ src/System/IO/Linear.hs
@@ -1,68 +1,122 @@
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE RebindableSyntax #-}
+{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE RoleAnnotations #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE UnboxedTuples #-}
 
--- | This module defines a resource-safe linear IO monad. It provide facilities
--- to add in your own resources.
+-- | This module redefines 'IO' with linear types. It defines a drop-in
+-- replacement for 'System.IO.IO' in "System.IO". This module will be deprecated
+-- if the definition for 'IO' found here is upstreamed in "System.IO".
+
+module System.IO.Linear
+  ( IO(..)
+  , fromSystemIO
+  , withLinearIO
+  -- * Monadic primitives
+  -- $monad
+  , BuilderType(..)
+  , builder
+  , return
+  -- * Exceptions
+  -- $exceptions
+  , throwIO
+  , catch
+  ) where
+
+import Control.Exception (Exception)
+import qualified Control.Exception as System (throwIO, catch)
+import GHC.Exts (State#, RealWorld)
+import Prelude.Linear hiding (IO, return)
+import qualified Unsafe.Linear as Unsafe
+import qualified System.IO as System
+
+-- | Like the standard IO monad, but as a linear state monad. Thanks to the
+-- linear arrow, we can safely expose the internal representation.
+newtype IO a = IO (State# RealWorld ->. (# State# RealWorld, a #))
+type role IO representational
+
+unIO :: IO a ->. State# RealWorld ->. (# State# RealWorld, a #)
+-- Defined separately because projections from newtypes are considered like
+-- general projections of data types, which take an unrestricted argument.
+unIO (IO action) = action
+
+-- | Coerces a standard IO action into a linear IO action
+fromSystemIO :: System.IO a ->. IO a
+-- The implementation relies on the fact that the monad abstraction for IO
+-- actually enforces linear use of the @RealWorld@ token.
 --
--- Functions in this module are meant to be qualified.
+-- There are potential difficulties coming from the fact that usage differs:
+-- returned value in 'System.IO' can be used unrestrictedly, which is not
+-- typically possible of linear 'IO'. This means that 'System.IO' action are
+-- not actually mere translations of linear 'IO' action. Still I [aspiwack]
+-- think that it is safe, hence no "unsafe" in the name.
+fromSystemIO = Unsafe.coerce
 
--- XXX: This would be better as a multiplicity-parametric relative monad, but
--- until we have multiplicity polymorphism, we use a linear monad.
+-- TODO:
+-- unrestrictedOfIO :: System.IO a -> IO (Unrestricted a)
+-- Needs an unsafe cast @a ->. Unrestricted a@
 
-module System.IO.Linear where
+toSystemIO :: IO (Unrestricted a) -> System.IO (Unrestricted a)
+toSystemIO = Unsafe.coerce -- basically just subtyping
 
-import qualified Data.IntMap.Strict as IntMap
-import Data.IntMap.Strict (IntMap)
-import Prelude.Linear hiding (IO)
-import qualified System.IO.Linear.Internal as Internal
+-- | Use at the top of @main@ function in your program to switch to the linearly
+-- typed version of 'IO':
+--
+-- @
+-- main :: IO ()
+-- main = withLinearIO $ do
+--     ...
+-- @
+withLinearIO :: IO (Unrestricted a) -> System.IO a
+withLinearIO action = unUnrestricted <$> (toSystemIO action)
 
-newtype ReleaseMap = ReleaseMap (IntMap (Internal.IO ()))
+-- $monad
 
-newtype IO a = IO {
-  unIO
-    :: ReleaseMap
-    -> Internal.IO (a, Unrestricted ReleaseMap)
+return :: a ->. IO a
+return a = IO $ \s -> (# s, a #)
+
+-- TODO: example of builder
+
+-- | Type of 'Builer'
+data BuilderType = Builder
+  { (>>=) :: forall a b. IO a ->. (a ->. IO b) ->. IO b
+  , (>>) :: forall b. IO () ->. IO b ->. IO b
   }
 
--- * Creating new types of resources
-
--- | The type of resources. Each safe resource is implemented as an abstract
--- newtype wrapper around @Resource R@ where @R@ is the unsafe variant of the
--- resource.
-data UnsafeResource a where
-  UnsafeResource :: Int -> a -> UnsafeResource a
-  -- Note that both components are unrestricted.
-
-unsafeRelease :: UnsafeResource a -> IO ()
-unsafeRelease (UnsafeResource key _) = IO (releaseWith key)
-  where
-    releaseWith key (ReleaseMap releaseMap) = do
-        releaser
-        Internal.return ((), Unrestricted (ReleaseMap nextMap))
+-- | A builder to be used with @-XRebindableSyntax@ in conjunction with
+-- @RecordWildCards@
+builder :: BuilderType
+builder =
+  let
+    (>>=) :: forall a b. IO a ->. (a ->. IO b) ->. IO b
+    x >>= f = IO $ \s ->
+        cont (unIO x s) f
       where
-        Internal.Builder {..} = Internal.builder -- used in the do-notation
-        releaser = releaseMap IntMap.! key
-        nextMap = IntMap.delete key releaseMap
+        -- XXX: long line
+        cont :: (# State# RealWorld, a #) ->. (a ->. IO b) ->. (# State# RealWorld, b #)
+        cont (# s', a #) f = unIO (f a) s'
 
--- XXX: long lines
-unsafeAquire
-  :: Internal.IO (Unrestricted a)
-  -> (a -> Internal.IO ())
-  -> IO (UnsafeResource a)
-unsafeAquire acquire release = IO $ \releaseMap -> do
-    Unrestricted resource <- acquire
-    makeRelease releaseMap resource
-  where
-    Internal.Builder {..} = Internal.builder -- used in the do-notation
-    makeRelease (ReleaseMap releaseMap) resource =
-        Internal.return (UnsafeResource releaseKey resource, Unrestricted (ReleaseMap nextMap))
+    (>>) :: forall b. IO () ->. IO b ->. IO b
+    x >> y = IO $ \s ->
+        cont (unIO x s) y
       where
-        releaseKey =
-          case IntMap.null releaseMap of
-            True -> 0
-            False -> fst (IntMap.findMax releaseMap) + 1
-        releaseAction =
-          release resource
-        nextMap =
-          IntMap.insert releaseKey releaseAction releaseMap
+        cont :: (# State# RealWorld, () #) ->. IO b ->. (# State# RealWorld, b #)
+        cont (# s', () #) y = unIO y s'
+
+  in
+    Builder{..}
+
+-- $exceptions
+--
+-- Note that the types of @throw@ and @catch@ sport only unrestricted arrows.
+-- Having any of the arrows be linear is unsound.
+
+throwIO :: Exception e => e -> IO a
+throwIO e = fromSystemIO $ System.throwIO e
+
+catch
+  :: Exception e
+  => IO (Unrestricted a) -> (e -> IO (Unrestricted a)) -> IO (Unrestricted a)
+catch body handler =
+  fromSystemIO $ System.catch (toSystemIO body) (\e -> toSystemIO (handler e))
