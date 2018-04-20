4c1ef8d62385031d71e6de3030b2ec5ea4eb4fba
Matthew Pickering <matthewtpickering@gmail.com>
2018-04-20 15:54:35 +0000

    Use prelude $ rather than the linear $

--- src/Foreign/Marshal/Pure.hs
+++ src/Foreign/Marshal/Pure.hs
@@ -58,9 +58,10 @@ import Foreign.Marshal.Utils
 import Foreign.Ptr
 import Foreign.Storable
 import Foreign.Storable.Tuple ()
-import Prelude.Linear
+import Prelude.Linear hiding (($))
 import System.IO.Unsafe
 import qualified Unsafe.Linear as Unsafe
+import Prelude (($))
 
 -- XXX: [2018-02-09] I'm having trouble with the `constraints` package (it seems
 -- that the version of Type.Reflection.Unsafe in the linear ghc compiler is not
