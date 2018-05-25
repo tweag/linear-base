34fbf7935c6b85dac872d639436eb494bacd2800
Arnaud Spiwack <arnaud@spiwack.net>
2018-05-25 19:36:09 +0200

    WIP: example optics
    [skip ci]

--- src/Control/Optics/Linear/Internal.hs
+++ src/Control/Optics/Linear/Internal.hs
@@ -7,9 +7,10 @@
 
 module Control.Optics.Linear.Internal where
 
-import Prelude.Linear
-import Data.Profunctor.Linear
+import qualified Data.Profunctor.Linear as Monoidal
+import Data.Profunctor.Linear hiding (swap)
 import Data.Void
+import Prelude.Linear
 
 type Optic_ arr a b s t = a `arr` b -> s `arr` t
 
@@ -17,8 +18,11 @@ type Optic c a b s t =
   forall arr. c arr => Optic_ arr a b s t
 
 type Iso a b s t = Optic Profunctor a b s t
+type Iso' a s = Iso a a s s
 type Lens a b s t = Optic (Strong (,) ()) a b s t
+type Lens' a s = Lens a a s s
 type Prism a b s t = Optic (Strong Either Void) a b s t
+type Prism' a s = Prism a a s s
 
 -- | Because it is inconvenient to pair up parametric constraints, 'Traversal'
 -- is defined in terms of 'Optic_' rather than 'Optic'.
@@ -26,3 +30,12 @@ type Traversal a b s t =
   forall arr.
   ( Strong (,) () arr , Strong Either Void arr , Monoidal (,) () arr)
   => Optic_ arr a b s t
+type Traversal' a s = Traversal a a s s
+
+-- TODO: I'm sure most of these type can be monomorphic optics into polymorphic
+-- ones.
+swap :: SymmetricMonoidal m u => Iso' (a `m` b) (b `m` a)
+swap = dimap Monoidal.swap Monoidal.swap
+
+_1 :: Lens' (a, b) a
+_1 p = _
