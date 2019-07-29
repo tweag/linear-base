1c67291a7ee5c86afafb99dcc238898628a2dbbf
Bhavik Mehta <bhavikmehta8@gmail.com>
2019-07-29 13:34:03 +0100

    Remove old pragma and old comment

--- src/System/IO/Linear.hs
+++ src/System/IO/Linear.hs
@@ -2,7 +2,6 @@
 {-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE LinearTypes #-}
 {-# LANGUAGE MagicHash #-}
-{-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE RebindableSyntax #-}
