d869e3dbb1ae29ea64bb4a4b495899bf8b58f7ac
Bhavik Mehta <bhavikmehta8@gmail.com>
2019-07-31 12:45:37 +0100

    Add monoidal associators

--- src/Data/Bifunctor/Linear.hs
+++ src/Data/Bifunctor/Linear.hs
@@ -9,9 +9,6 @@ module Data.Bifunctor.Linear where
 import Prelude.Linear
 import Data.Void
 
--- TODO: associators
--- TODO: remove `swap` from Prelude
-
 -- | Bifunctors on the category of linear types
 --
 -- Laws:
@@ -44,16 +41,27 @@ instance Bifunctor Either where
   bimap f _ (Left x) = Left (f x)
   bimap _ g (Right y) = Right (g y)
 
+-- TODO: assoc laws
 -- | Symmetric monoidal products on the category of linear types
 --
 -- Laws
 -- * @'swap' . 'swap' = 'id'@
 class Bifunctor m => SymmetricMonoidal (m :: * -> * -> *) (u :: *) | m -> u, u -> m where
+  {-# MINIMAL swap, (assoc | unassoc) #-}
+  assoc :: (a `m` b) `m` c ->. a `m` (b `m` c)
+  assoc = swap . unassoc . swap . unassoc . swap
+  unassoc :: a `m` (b `m` c) ->. (a `m` b) `m` c
+  unassoc = swap . assoc . swap . assoc . swap
   swap :: a `m` b ->. b `m` a
+-- XXX: should unitors be added?
 
 instance SymmetricMonoidal (,) () where
   swap (x, y) = (y, x)
+  assoc ((x,y),z) = (x,(y,z))
 
 instance SymmetricMonoidal Either Void where
   swap (Left x) = Right x
   swap (Right x) = Left x
+  assoc (Left (Left x)) = Left x
+  assoc (Left (Right x)) = Right (Left x)
+  assoc (Right x) = Right (Right x)
