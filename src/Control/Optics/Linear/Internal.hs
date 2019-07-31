6c99c653e352ff585dc8dfc512ad31c6589fc676
Bhavik Mehta <bhavikmehta8@gmail.com>
2019-07-31 18:59:54 +0100

    Optic newtype and more polymorphic examples

--- src/Control/Optics/Linear/Internal.hs
+++ src/Control/Optics/Linear/Internal.hs
@@ -12,7 +12,7 @@ import Data.Profunctor.Linear hiding (swap)
 import Data.Void
 import Prelude.Linear
 
-type Optic_ arr a b s t = a `arr` b -> s `arr` t
+newtype Optic_ arr a b s t = Optical (a `arr` b -> s `arr` t)
 
 type Optic c a b s t =
   forall arr. c arr => Optic_ arr a b s t
@@ -26,28 +26,31 @@ type Prism' a s = Prism a a s s
 
 -- | Because it is inconvenient to pair up parametric constraints, 'Traversal'
 -- is defined in terms of 'Optic_' rather than 'Optic'.
-type Traversal a b s t =
-  forall arr.
-  ( Strong (,) () arr , Strong Either Void arr , Monoidal (,) () arr)
-  => Optic_ arr a b s t
-type Traversal' a s = Traversal a a s s
+-- type Traversal a b s t =
+--   forall arr.
+--   ( Strong (,) () arr , Strong Either Void arr , Monoidal (,) () arr)
+--   => Optic_ arr a b s t
+-- type Traversal' a s = Traversal a a s s
 
 -- TODO: I'm sure most of these type can be monomorphic optics into polymorphic
 -- ones.
-swap :: SymmetricMonoidal m u => Iso' (a `m` b) (b `m` a)
-swap = dimap Monoidal.swap Monoidal.swap
+swap :: SymmetricMonoidal m u => Iso (a `m` b) (c `m` d) (b `m` a) (d `m` c)
+swap = Optical (dimap Monoidal.swap Monoidal.swap)
 
-_1 :: Lens' a (a, b)
-_1 = first
+assoc :: SymmetricMonoidal m u => Iso ((a `m` b) `m` c) ((d `m` e) `m` f) (a `m` (b `m` c)) (d `m` (e `m` f))
+assoc = Optical (dimap lassoc rassoc)
 
-_2 :: Lens' b (a, b)
-_2 = second
+_1 :: Lens a b (a,c) (b,c)
+_1 = Optical first
 
-_Left :: Prism' a (Either a b)
-_Left = first
+_2 :: Lens a b (c,a) (c,b)
+_2 = Optical second
 
-_Right :: Prism' b (Either a b)
-_Right = second
+_Left :: Prism a b (Either a c) (Either b c)
+_Left = Optical first
+
+_Right :: Prism a b (Either c a) (Either c b)
+_Right = Optical second
 
 -- traversed :: Traversable t => Traversal a b (t a) (t b)
 -- traversed p = _
