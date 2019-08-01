62dbcffec0825a82a20159536e91e28039ea1179
Bhavik Mehta <bhavikmehta8@gmail.com>
2019-08-01 17:52:29 +0100

    A selection of lens consumers and new profunctors

--- src/Control/Optics/Linear/Internal.hs
+++ src/Control/Optics/Linear/Internal.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE LinearTypes #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeOperators #-}
@@ -9,10 +10,13 @@ module Control.Optics.Linear.Internal where
 
 import qualified Data.Bifunctor.Linear as Bifunctor
 import Data.Bifunctor.Linear (SymmetricMonoidal)
+import Data.Functor.Const
 import Data.Functor.Linear
+import Data.Monoid
 import Data.Profunctor.Linear
 import Data.Void
 import Prelude.Linear
+import qualified Prelude as P
 
 newtype Optic_ arr a b s t = Optical (a `arr` b -> s `arr` t)
 
@@ -48,3 +52,24 @@ _Right = Optical second
 
 traversed :: Traversable t => Traversal a b (t a) (t b)
 traversed = Optical wander
+
+over :: Optic_ LinearArrow a b s t -> (a ->. b) -> s ->. t
+over (Optical l) f = getLA (l (LA f))
+
+traverseOf :: Optic_ (LKleisli f) a b s t -> (a ->. f b) -> s ->. f t
+traverseOf (Optical l) f = runLKleisli (l (LKleisli f))
+
+get :: Optic_ (Kleisli (Const a)) a b s t -> s -> a
+get l = gets l P.id
+
+gets :: Optic_ (Kleisli (Const r)) a b s t -> (a -> r) -> s -> r
+gets (Optical l) f s = getConst (runKleisli (l (Kleisli (Const P.. f))) s)
+
+set :: Optic_ (->) a b s t -> b -> s -> t
+set (Optical l) x = l (const x)
+
+lengthOf :: Num r => Optic_ (Kleisli (Const (Sum r))) a b s t -> s -> r
+lengthOf l s = getSum (gets l (const (Sum 1)) s)
+
+traverseOf' :: Optic_ (Kleisli f) a b s t -> (a -> f b) -> s -> f t
+traverseOf' (Optical l) f = runKleisli (l (Kleisli f))
