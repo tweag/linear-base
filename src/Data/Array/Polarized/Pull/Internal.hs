0fb34fd81a78924416167c29ad11ebd598002c85
Bhavik Mehta <bhavikmehta8@gmail.com>
2019-08-16 14:03:04 +0100

    Changes from review

--- src/Data/Array/Polarized/Pull/Internal.hs
+++ src/Data/Array/Polarized/Pull/Internal.hs
@@ -16,17 +16,13 @@ import qualified Unsafe.Linear as Unsafe
 
 -- | A pull array represents arrays which are easy to extract elements from.
 -- They provide an indexing function, and a length. If the indexing function
--- is called at a point out of range, the result is unspecified.
+-- is called at a point out of range, the result is an error.
 data Array a where
   Array :: (Int -> a) -> Int -> Array a
--- Int is movable, so this is the same as Array :: (Int -> a) -> ...
+-- Int is movable, so this is the same as Array :: (Int ->. a) -> ...
 
 instance Data.Functor Array where
-  fmap f (Array g n) = Array (\x -> f (g x)) n
-
--- XXX: This doesn't match how PullArrays should work, but it is accepted
--- ap :: Array (x ->. y) ->. Array x ->. Array y
--- ap (Array g n) (Array h m) = Array (\k -> let (q,r) = k `quotRem` n in g q (h r)) (n*m)
+  fmap f (Array g n) = fromFunction (\x -> f (g x)) n
 
 -- XXX: This should be well-typed without the unsafe, but it isn't accepted: the
 -- PullArray type probably isn't the ideal choice
@@ -34,20 +30,20 @@ instance Data.Functor Array where
 -- taken out of the Array (which is interesting in and of itself: I think this
 -- is like an n-ary With), and changing the other arrows makes no difference)
 singleton :: a ->. Array a
-singleton = Unsafe.toLinear (\x -> Array (\_ -> x) 1)
+singleton = Unsafe.toLinear (\x -> fromFunction (\_ -> x) 1)
 
 -- | /!\ Partial! Only works if both arrays have the same length.
 zip :: Array a ->. Array b ->. Array (a,b)
 zip (Array g n) (Array h m)
   | n /= m    = error "Polarized.zip: size mismatch"
-  | otherwise = Array (\k -> (g k, h k)) n
+  | otherwise = fromFunction (\k -> (g k, h k)) n
 
 append :: Array a ->. Array a ->. Array a
-append (Array f m) (Array g n) = Array (\k -> if k < m then f k else g k) (m + n)
+append (Array f m) (Array g n) = Array (\k -> if k < m then f k else g (k-m)) (m + n)
 
 -- | Creates a pull array of given size, filled with the given element
 make :: a -> Int -> Array a
-make x n = Array (const x) n
+make x n = fromFunction (const x) n
 
 -- TODO: Use a derivingvia combinator to omit this
 instance Prelude.Semigroup (Array a) where
@@ -66,8 +62,26 @@ foldr f z (Array g n) = go f z g n
 findLength :: Array a ->. (Int, Array a)
 findLength (Array f n) = (n, Array f n)
 
+-- | A smart constructor for PullArrays
 fromFunction :: (Int -> a) -> Int -> Array a
-fromFunction = Array
+fromFunction f n = Array f' n
+  where f' k
+          | k < 0 = error "Pull.Array: negative index"
+          | k >= n = error "Pull.Array: index too large"
+          | otherwise = f k
+-- XXX: this is used internally to ensure out of bounds errors occur, but
+-- is unnecessary if the input function can be assumed to already have bounded
+-- domain
 
 toVector :: Array a ->. Vector a
 toVector (Array f n) = Vector.generate n f
+
+-- | @'split' n v = (vl, vr)@ such as @vl@ has length @n@.
+--
+-- 'split' is total: if @n@ is larger than the length of @v@, then @vr@ is
+-- empty.
+split :: Int -> Array a ->. (Array a, Array a)
+split k (Array f n) = (fromFunction f (min k n), fromFunction (\x -> f (x+k)) (max (n-k) 0))
+
+reverse :: Array a ->. Array a
+reverse (Array f n) = Array (\x -> f (n+1-x)) n
