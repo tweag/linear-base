b2d9c0ed63e361ad0ae1afa5981381df3ebdd0d8
Arnaud Spiwack <arnaud@spiwack.net>
2020-01-17 18:52:25 +0100

    fixup! Setup buildkite and nix.

--- shell-stack.nix
+++ shell-stack.nix
@@ -5,7 +5,7 @@
 with pkgs;
 
 haskell.lib.buildStackProject {
-  name = "inline-java";
+  name = "linear-base";
   buildInputs = [ git gradle zlib ];
   ghc = pkgs.haskell.compiler.ghcLinearTypes20191220;
   LANG = "en_US.utf8";
