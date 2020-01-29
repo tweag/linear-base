ea354b3eb370415546b1e41f79f391922b9ec2e5
Facundo Domínguez <facundo.dominguez@tweag.io>
2020-01-29 14:31:57 +0000

    Implement MonadIO as a type-class.

--- src/Control/Monad/IO/Class/Linear.hs
+++ src/Control/Monad/IO/Class/Linear.hs
@@ -1,21 +1,15 @@
-{-# LANGUAGE ConstraintKinds #-}
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LinearTypes #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 module Control.Monad.IO.Class.Linear where
 
-import qualified Control.Monad.IO.Class as NonLinear
-import Control.Monad.Lift.Linear
+import qualified Control.Monad.Linear as Linear
 import Prelude.Linear
+import qualified System.IO.Linear as Linear
 
--- | Like 'NonLinear.MonadIO' but allows to lift non-linear 'IO'
+-- | Like 'NonLinear.MonadIO' but allows to lift linear 'IO'
 -- actions into a linear monad
-type MonadIO m = (MonadLift m, NonLinear.MonadIO (LiftedM m))
+class Linear.Monad m => MonadIO m where
+  liftIO :: Linear.IO a ->. m a
 
--- | Like 'NonLinear.liftIO' but allows to lift non-linear 'IO'
--- actions into a linear monad
-liftIO :: MonadIO m => IO a -> m a
-liftIO io = lift (NonLinear.liftIO io)
-
--- | Like 'liftIO' but allows to use the return value unrestrictedly
-liftIOU :: MonadIO m => IO a -> m (Unrestricted a)
-liftIOU io = liftU (NonLinear.liftIO io)
+instance MonadIO Linear.IO where
+  liftIO = id
