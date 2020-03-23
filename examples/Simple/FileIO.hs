1e8447d334a284042567291663ec373b18a4dcf3
Divesh Otwani <divesh.otwani@gmail.com>
2020-03-23 11:44:38 +0100

    Ran ormolu on FileIO.hs

--- examples/Simple/FileIO.hs
+++ examples/Simple/FileIO.hs
@@ -1,47 +1,39 @@
+{-# LANGUAGE LinearTypes #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE RebindableSyntax #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeInType #-}
+{-# LANGUAGE TypeOperators #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 
-{-# LANGUAGE LinearTypes         #-}
-{-# LANGUAGE RebindableSyntax    #-}
-{-# LANGUAGE RecordWildCards     #-}
-{-# LANGUAGE TypeOperators       #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeInType          #-}
-{-# LANGUAGE TypeApplications    #-}
-{-# LANGUAGE GADTs               #-}
-{-# LANGUAGE TypeFamilies        #-}
-{-# LANGUAGE FlexibleInstances   #-}
-{-# LANGUAGE StandaloneDeriving  #-}
-
-
-{-|
-Module      : FileIO
-Description : The Linear File IO example from the Linear Haskell paper.
-
-We implement a function that prints the first line of a file.
-
-We do this with the normal file IO interface in base and the linear file IO
-interface in linear-base. With the latter, the protocol for using files is
-enforced by the linear type system. For instance, forgetting to close the file
-will induce a type error at compile time. That is, typechecking proves that all
-opened files are closed at some later point in execution. With the former
-interface, we have code that type checks but will error or cause errors at
-runtime.
-
--}
-
-
+-- |
+-- Module      : FileIO
+-- Description : The Linear File IO example from the Linear Haskell paper.
+--
+-- We implement a function that prints the first line of a file.
+--
+-- We do this with the normal file IO interface in base and the linear file IO
+-- interface in linear-base. With the latter, the protocol for using files is
+-- enforced by the linear type system. For instance, forgetting to close the file
+-- will induce a type error at compile time. That is, typechecking proves that all
+-- opened files are closed at some later point in execution. With the former
+-- interface, we have code that type checks but will error or cause errors at
+-- runtime.
 module Simple.FileIO where
 
-import Prelude
-import qualified System.IO as System
 import Control.Monad ()
-import Data.Text
-
 -- Linear Base Imports
 import qualified Control.Monad.Linear.Builder as Control
-import qualified System.IO.Resource as Linear
+import Data.Text
 import Data.Unrestricted.Linear
-
+import qualified System.IO as System
+import qualified System.IO.Resource as Linear
+import Prelude
 
 -- *  Non-linear first line printing
 --------------------------------------------
@@ -59,7 +51,6 @@ printFirstLine fpath = do
   System.putStrLn firstLine
   System.hClose fileHandle
 
-
 -- This compiles but can cause issues!
 -- The number of file handles you can have active is finite and after that
 -- openFile errors. This is especially critical on mobile devices or systems
@@ -70,7 +61,6 @@ printFirstLineNoClose fpath = do
   firstLine <- System.hGetLine fileHandle
   System.putStrLn firstLine
 
-
 -- This compiles, but will throw an error!
 printFirstLineAfterClose :: FilePath -> System.IO ()
 printFirstLineAfterClose fpath = do
@@ -79,24 +69,22 @@ printFirstLineAfterClose fpath = do
   firstLine <- System.hGetLine fileHandle
   System.putStrLn firstLine
 
-
 -- * Linear first line printing
 --------------------------------------------
 
 linearGetFirstLine :: FilePath -> RIO (Unrestricted Text)
 linearGetFirstLine fp = do
-    handle <- Linear.openFile fp System.ReadMode
-    (t, handle') <- Linear.hGetLine handle
-    Linear.hClose handle'
-    return t
+  handle <- Linear.openFile fp System.ReadMode
+  (t, handle') <- Linear.hGetLine handle
+  Linear.hClose handle'
+  return t
   where
-    Control.Builder{..} = Control.monadBuilder
+    Control.Builder {..} = Control.monadBuilder
 
 linearPrintFirstLine :: FilePath -> System.IO ()
 linearPrintFirstLine fp = do
   text <- Linear.run (linearGetFirstLine fp)
   System.putStrLn (unpack text)
-
 
 {-
     For clarity, we show this function without do notation.
@@ -119,6 +107,7 @@ linearPrintFirstLine fp = do
 
 -- Some type synonyms
 type RIO = Linear.RIO
+
 type LinHandle = Linear.Handle
 
 -- | Linear bind
@@ -127,7 +116,7 @@ type LinHandle = Linear.Handle
 (>>#=) :: RIO a #-> (a #-> RIO b) #-> RIO b
 (>>#=) = (>>=)
   where
-    Control.Builder{..} = Control.monadBuilder
+    Control.Builder {..} = Control.monadBuilder
 
 -- | Non-linear bind
 -- Notice the continuation has a non-linear arrow,
@@ -137,36 +126,32 @@ type LinHandle = Linear.Handle
 (>>==) :: RIO () #-> (() -> RIO b) #-> RIO b
 (>>==) ma f = ma >>= (\() -> f ())
   where
-    Control.Builder{..} = Control.monadBuilder
+    Control.Builder {..} = Control.monadBuilder
 
 -- | Inject
 -- provided just to make the type explicit
 inject :: a #-> RIO a
 inject = return
   where
-    Control.Builder{..} = Control.monadBuilder
-
+    Control.Builder {..} = Control.monadBuilder
 
 -- * The explicit example
 -------------------------------------------------
 
 getFirstLineExplicit :: FilePath -> RIO (Unrestricted Text)
 getFirstLineExplicit path =
-  (openFileForReading path) >>#=
-    readOneLine >>#=
-      closeAndReturnLine  -- Internally uses (>>==)
+  (openFileForReading path)
+    >>#= readOneLine
+    >>#= closeAndReturnLine -- Internally uses (>>==)
   where
     openFileForReading :: FilePath -> RIO LinHandle
     openFileForReading fp = Linear.openFile fp System.ReadMode
-
     readOneLine :: LinHandle #-> RIO (Unrestricted Text, LinHandle)
     readOneLine = Linear.hGetLine
-
     closeAndReturnLine ::
       (Unrestricted Text, LinHandle) #-> RIO (Unrestricted Text)
-    closeAndReturnLine (unrText,handle) =
+    closeAndReturnLine (unrText, handle) =
       Linear.hClose handle >>#= (\() -> inject unrText)
-
 
 printFirstLineExplicit :: FilePath -> System.IO ()
 printFirstLineExplicit fp = do
