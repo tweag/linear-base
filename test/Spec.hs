5155915a6afcf89f8aeaa754c1de39002043560f
Divesh Otwani <divesh.otwani@gmail.com>
2020-04-10 11:22:32 +0200

    Trying to fix CI

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 
-import Hedgehog
-
 import Test.Data.Mutable.Array (mutArrTests)
 import Test.Data.Mutable.Vector (mutVecTests)
 import Test.Data.Mutable.HashMap (mutHMTests)
