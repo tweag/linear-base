6474dbe21bde8f54c2207931455c430bd7cbdfb6
Divesh Otwani <divesh.otwani@gmail.com>
2020-05-12 13:25:15 +0200

    fixup! Documentation of linear lenses

--- src/Control/Optics/Linear/Internal.hs
+++ src/Control/Optics/Linear/Internal.hs
@@ -96,7 +96,7 @@
 --
 -- == Appendix: How do the types make sense?
 --
--- === At a technical level...
+-- === What are the optic types and why?
 --
 -- All optics are functions, called __optic functions__ of the type
 --
@@ -105,38 +105,70 @@
 -- where @arr@ is some specific kind of arrow. Note that is is a rank-2 type
 -- (which just means that there is a @forall@ nested inside a @forall@).
 --
--- TODO: rewrite the following for the types here
+-- The type of each optic suffices to write functions that go from the optics
+-- in this module to the standard optic types in the
+-- [lens package](https://hackage.haskell.org/package/lens).
 --
--- Any @Lens@ is a @Traversal@. A @Lens@ is a function that can be applied
--- for /any/ functor. A @Traversal@ is a function that can be applied for /any/
--- applicative functor. (Recall that these are rank-2 types.) An applicative
--- functor is a specific kind of functor. Hence, if I have a function that can
--- be applied for any functor, and I provide arguments that feature an
--- applicative functor, that works. Put another way, a @Lens@ is a more generic
--- function than a @Traversal@ since a @Traversal@ has a stricter constraint,
--- i.e., @Applicative@ is stricter (and includes) @Functor@.  
--- * Any @Prism@ is a @Traversal@. A @Prism@ has a stricter condition: the
--- optic function must work for /any/ @Choice c@. A traversal's optic function
--- works for the function arrow @->@, which is an instance of @Choice@.
+-- === How do these types imply the sub-typing diagram above?
 --
--- Please take care to understand the principle at play:
+-- Let's look at a simple example and then describe the core principle
+-- that explains the sub-typing diagram above.
 --
--- __If @D@ is a stricter constraint than @C@ (i.e., any @D@ instance has a @C@
--- instance), then the rank-2 type @forall x. C x => f@ is a special case of
--- @forall x. D x => f@__
+-- The example: any @Iso a b s t@ is a @Lens a b s t@.
+-- The types are essentially these:
 --
--- Hence, we create specific kinds of Traversals by relaxing the constraints of
--- Traversals.
+-- > type Iso a b s t  = forall p. Profunctor p      => a `p` b -> s `p` t
+-- > type Lens a b s t = forall p. (Strong (,) () p) => a `p` b -> s `p` t
 --
--- === More abstractly ...
+-- By the definition of @Strong@ in "Data.Profunctor.Linear", 
+-- any @p@ with a @Strong m u p@ instance also has a @Profunctor p@ instance.
+--
+-- Hence, we can write
+--
+-- > convert :: Iso a b s t -> Lens a b s t
+-- > convert iso a_p_b = iso a_p_b
+--
+-- This works because these are rank-2 types. The @iso@ is an optic function
+-- that transforms any profunctor computation of type 
+-- @Profunctor p => a `p` b@.  The second argument to @convert@ is @a_p_b@ which
+-- is a computation of type @Strong (,) () p => a `p` b@. However, since the
+-- @Strong@ constraint implies @Profunctor@ constraint, we can give
+-- @a_p_b@ to @iso@. We could not write @convert@ with the types going the
+-- other way.
+--
+-- More generally, since isomorphisms are functions that work on all
+-- profunctors, they are also functions that work on all profunctors
+-- with additional constraints. Moreover, there are more optic functions that
+-- work on all profunctors with that @Strong@ constraint since the assumption
+-- of futher constraints allows other optic functions to be written.  This is
+-- analogus to how in abstract algebra, a theorem on groups is still a theorem
+-- about abelian groups and in fact, the theorems that hold on abelian groups
+-- are a superset of the theorems that hold on just groups.
+--
+-- __Please take care to understand the principle at play:__
+--
+-- /If @D@ is a stricter constraint than @C@ (i.e., any @D@ instance has a @C@
+-- instance), then the rank-2 type @forall x. C x => f@ is a sub-type of
+-- @forall x. D x => f@/
+--
+-- === What do these optic types /mean/?
 --
 -- This is quite complicated. With simple types like a @Maybe a@ or @Monad m =>
--- a -> m b@, we can develop a sense of what the type means abstractly.  This
+-- a -> m b@, we can develop a sense of what the type means abstractly. This
 -- is much harder with optics because of all the type classes and the rank-2
 -- types.
 --
--- TODO
+-- Even classic lens type, for instance, is not easily grasped abtractly:
 --
+-- > type Lens s a = forall f. Functor f => (a -> f a) -> (s -> f s)
+--
+-- So, suffice it to say that the types of linear optics are simply generic
+-- enough to making the interface we want, i.e., providing a first class object
+-- with which we can map over sub-structure(s) in a structure and view
+-- sub-structure(s) in a structure.
+--
+-- However, due to the fact that they are so general, they have several other
+-- unforseen use cases.
 module Control.Optics.Linear.Internal
   ( -- * Definitions of Optics
     Optic_(..)
@@ -194,19 +226,41 @@ newtype Optic_ arr a b s t = Optical (a `arr` b -> s `arr` t)
 type Optic c a b s t =
   forall arr. c arr => Optic_ arr a b s t
 
--- | TODO
+-- | An isomorphism between @s@ and @t@ represents the fact that
+-- except for changing each sub-structure of type @a@ into type @b@,
+-- these structures are algebraically the same.
+--
+-- For instance, @Either () a@ and @Maybe a@ are isomorphic.
+--
+-- See [the wiki](https://en.wikipedia.org/wiki/Isomorphism).
 type Iso a b s t = Optic Profunctor a b s t
 type Iso' a s = Iso a a s s
 
--- | TODO
+-- | A lens is something that focuses on a single sub-structure
+-- of type @a@ in the structure of product type @s@.
+--
+-- Essentially, there exists some @x@ such that
+-- @s@ is isomorphic to @(a,x)@. A lens provides
+-- a way to get the @a@ and map over it to make a
+-- @(b,x)@ which is isomorphic to @t@
 type Lens a b s t = Optic (Strong (,) ()) a b s t
 type Lens' a s = Lens a a s s
 
--- | TODO
+-- | A prism is something that focuses on a branch of a sum type @s@.
+--
+-- Essentially, there exists some x such that @s@ is isomorphic to
+-- @Either a x@. A prism provides a way to access the @a@ (if it exists)
+-- and mapping to create a @Either b x@ which is isomorphic to a @t@.
 type Prism a b s t = Optic (Strong Either Void) a b s t
 type Prism' a s = Prism a a s s
 
--- | TODO
+-- | A traversal is something that focuses on several sub-structures of
+-- a product type.
+--
+-- Essentially, for some structure @s@ there is some type @x@ such that
+-- @s@ is isomorphic to @([a], x)@. A traversal provides a way to get out
+-- the list of type @[a]@ and to map over @s@ to create a structure of type
+-- @t@ which is isomorphic to @([b], x)@.
 type Traversal a b s t = Optic Wandering a b s t
 type Traversal' a s = Traversal a a s s
 
@@ -219,9 +273,23 @@ Optical f .> Optical g = Optical (f P.. g)
 -- # Common Optics
 -------------------------------------------------------------------------------
 
+-- | An isomorphism of the symmetry symmetric monoidal bifunctors.
+-- A simple example: for the symmetric monoidal of a tuple @(,)@,
+-- with a simple @Iso'@, this is of type
+--
+-- > swap :: Iso' (a,b) (b,a)
+--
+-- which is clearly the case. These two types are isomorphic.
 swap :: SymmetricMonoidal m u => Iso (a `m` b) (c `m` d) (b `m` a) (d `m` c)
 swap = iso Bifunctor.swap Bifunctor.swap
 
+-- | An isomorphism of the associativity of a symmetric monoidal bifunctors.
+-- A simple example: for the symmetric monoidal of a tuple @(,)@,
+-- with a simple @Iso'@, this is of type
+--
+-- > assoc :: Iso' ((a,b),c)) (a,(b,c))
+--
+-- and these two types are surely isomorphic.
 assoc :: SymmetricMonoidal m u => Iso ((a `m` b) `m` c) ((d `m` e) `m` f) (a `m` (b `m` c)) (d `m` (e `m` f))
 assoc = iso Bifunctor.lassoc Bifunctor.rassoc
 
@@ -262,24 +330,34 @@ traversed = Optical wander
 -- #  Using Optics
 -------------------------------------------------------------------------------
 
+-- | Use a lens or prism to map a (linear) function over a structure
 over :: Optic_ LinearArrow a b s t -> (a #-> b) -> s #-> t
 over (Optical l) f = getLA (l (LA f))
 
+-- | Using a traversal, map actions over each sub-structure of type @a@ in a
+-- @s@ and create an action that produces a @t@
 traverseOf :: Optic_ (Linear.Kleisli f) a b s t -> (a #-> f b) -> s #-> f t
 traverseOf (Optical l) f = Linear.runKleisli (l (Linear.Kleisli f))
 
+-- | Using a lens, get or view a sub-structure of type @a@ in a structure
+-- of type @s@ with a lens of type @Lens s a@.
 get :: Optic_ (NonLinear.Kleisli (Const a)) a b s t -> s -> a
 get l = gets l P.id
 
+-- | Like 'get' but applies a function of type @a -> r@ before returning
 gets :: Optic_ (NonLinear.Kleisli (Const r)) a b s t -> (a -> r) -> s -> r
 gets (Optical l) f s = getConst' (NonLinear.runKleisli (l (NonLinear.Kleisli (Const P.. f))) s)
 
+-- | Using a lens, set the sub-structure of type @a@ in an @s@ to something of
+-- type @b@ that transforms the structure into a @t@.
 set :: Optic_ (->) a b s t -> b -> s -> t
 set (Optical l) x = l (const x)
 
+-- | Using a prism, break down a sum-type @s@ into cases
 match :: Optic_ (Market a b) a b s t -> s #-> Either t a
 match (Optical l) = snd (runMarket (l (Market id Right)))
 
+-- | Using a prism, build a value of a sum-type @t@ with one branch of type @b@
 build :: Optic_ (Linear.CoKleisli (Const b)) a b s t -> b #-> t
 build (Optical l) x = Linear.runCoKleisli (l (Linear.CoKleisli getConst')) (Const x)
 
@@ -294,9 +372,12 @@ lengthOf l s = getAdded (gets l (const (Adding one)) s)
 
 -- XXX: the below two functions will be made redundant with multiplicity
 -- polymorphism on over and traverseOf'
+
+-- | The same as 'over' but for normal non-linear arrows @(->)@
 over' :: Optic_ (->) a b s t -> (a -> b) -> s -> t
 over' (Optical l) f = l f
 
+-- | The same as 'traverseOf' but for non-linear Kleisi arrows
 traverseOf' :: Optic_ (NonLinear.Kleisli f) a b s t -> (a -> f b) -> s -> f t
 traverseOf' (Optical l) f = NonLinear.runKleisli (l (NonLinear.Kleisli f))
 
@@ -311,9 +392,16 @@ withPrism (Optical l) f = f b m
 -- # Constructing Optics
 -------------------------------------------------------------------------------
 
+-- | @iso convertForth convertBack@ constructs an isomorphism
+-- given functions to convert back and forth between @s@ and @t@.
 iso :: (s #-> a) -> (b #-> t) -> Iso a b s t
 iso f g = Optical (dimap f g)
 
+-- | @prism builder takeCase@ constructs a prism given a @builder@
+-- which goes from a @b@ (produced by applying a function of type
+-- @a -> b@ to the @a@ case of the sum type @s@) to a @t@ and a
+-- @takeCase@ which breaks @s@ down into either the @a@ case or the
+-- transformed structure of type @t@.
 prism :: (b #-> t) -> (s #-> Either t a) -> Prism a b s t
 prism b s = Optical $ \f -> dimap s (either id id) (second (rmap b f))
 
