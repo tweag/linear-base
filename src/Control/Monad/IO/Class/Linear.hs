fe1963c121302a9abca7388631a3b089d2d6c2fd
Facundo Domínguez <facundo.dominguez@tweag.io>
2020-07-09 15:13:54 +0000

    Add methods liftSystemIO/U to MonadIO.

--- src/Control/Monad/IO/Class/Linear.hs
+++ src/Control/Monad/IO/Class/Linear.hs
@@ -4,12 +4,17 @@ module Control.Monad.IO.Class.Linear where
 
 import qualified Control.Monad.Linear as Linear
 import Prelude.Linear
+import qualified System.IO as System
 import qualified System.IO.Linear as Linear
 
--- | Like 'NonLinear.MonadIO' but allows to lift linear 'IO'
--- actions into a linear monad
+-- | Like 'NonLinear.MonadIO' but allows to lift both linear
+-- and non-linear 'IO' actions into a linear monad.
 class Linear.Monad m => MonadIO m where
   liftIO :: Linear.IO a #-> m a
+  liftSystemIO :: System.IO a -> m a
+  liftSystemIO io = liftIO (Linear.fromSystemIO io)
+  liftSystemIOU :: System.IO a -> m (Unrestricted a)
+  liftSystemIOU io = liftIO (Linear.fromSystemIOU io)
 
 instance MonadIO Linear.IO where
   liftIO = id
