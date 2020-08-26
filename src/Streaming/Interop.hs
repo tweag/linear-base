4775334bba6b20eab08b2fe4ad0f020bb73da0bc
Divesh Otwani <divesh.otwani@gmail.com>
2020-08-26 06:59:47 -0400

    General Streaming.hs module implemented

--- src/Streaming/Interop.hs
+++ src/Streaming/Interop.hs
@@ -17,7 +17,12 @@ import Prelude (Maybe(..))
 import qualified Control.Monad.Linear as Control
 import Control.Monad.Linear.Builder (BuilderType(..), monadBuilder)
 
+{-| Read an @IORef (Maybe a)@ or a similar device until it reads @Nothing@.
+    @reread@ provides convenient exit from the @io-streams@ library
 
+> reread readIORef    :: IORef (Maybe a) -> Stream (Of a) IO ()
+> reread Streams.read :: System.IO.Streams.InputStream a -> Stream (Of a) IO ()
+-}
 reread :: Control.Monad m =>
   (s -> m (Unrestricted (Maybe a))) -> s -> Stream (Of a) m ()
 reread f s = Effect $ do
--- src/Streaming/Many.hs
+++ src/Streaming/Many.hs
@@ -9,12 +9,8 @@
 -- streams, splitting a stream, etc.
 module Streaming.Many
   (
-  -- * Zips and unzips
-    zip
-  , zipWith
-  , zip3
-  , zipWith3
-  , unzip
+  -- * Unzip
+    unzip
   -- * Merging
   , merge
   , mergeOn
@@ -30,107 +26,9 @@ import qualified Prelude.Linear as Linear
 import qualified Control.Monad.Linear as Control
 import Control.Monad.Linear.Builder (BuilderType(..), monadBuilder)
 
--- # Comments on designing zip functions
---
--- * Zip will not work on infinite length linear streams. We'll get stuck with
--- some tail that we can never consume.
--- * On finite length streams, we need to ask about how we consume
--- the remainder of the shorter stream and if the streams are equal,
--- what we do with the two end-of-stream values of type 'r'.
--- * There are two options that seem to make sense for finite length streams:
--- (1) return the remainder or the two 'r's, and (2) use 'effects' to consume
--- the remainder and return the two 'r's.
--- * We chose the last one since it most closely matches the Unrestricted
--- original version.
 
-
--- # Zips and Unzips
+-- # Unzip
 -------------------------------------------------------------------------------
-
--- | 'zipWith' exhausts the remainder of the longer stream and keeps
--- both end-of-stream values. Note: this will not terminate on
--- infinite streams.
-zipWith :: Control.Monad m =>
-  (a -> b -> c) ->
-  Stream (Of a) m r1 #->
-  Stream (Of b) m r2 #->
-  Stream (Of c) m (r1,r2)
-zipWith = loop
-  where
-  loop :: Control.Monad m =>
-    (a -> b -> c) ->
-    Stream (Of a) m r1 #->
-    Stream (Of b) m r2 #->
-    Stream (Of c) m (r1,r2)
-  loop f st1 st2 = st1 & \case
-    Effect ms -> Effect $ ms >>= (\s -> return $ zipWith f s st2)
-    Return r1 -> Effect $ (effects st2) >>= (\r2 -> return $ Return (r1, r2))
-    Step (a :> as) -> st2 & \case
-      Effect ms ->
-        Effect $ ms >>= (\s -> return $ zipWith f (Step (a :> as)) s)
-      Return r2 ->
-        Effect $ (effects as) >>= (\r1 -> return $ Return (r1, r2))
-      Step (b :> bs) -> Step $ (f a b) :> zipWith f as bs
-    where
-      Builder{..} = monadBuilder
-
--- | 'zip' exhausts the remainder of the longer stream and keeps
--- both end-of-stream values. Note: this will not terminate on
--- infinite streams.
-zip :: Control.Monad m =>
-  Stream (Of a) m r1 #->
-  Stream (Of b) m r2 #->
-  Stream (Of (a,b)) m (r1,r2)
-zip = zipWith (,)
-
--- | Like 'zipWith' but with three streams. Note: the remainders of the
--- two longer streams are exhausted.
-zipWith3 :: Control.Monad m =>
-  (a -> b -> c -> d) ->
-  Stream (Of a) m r1 #->
-  Stream (Of b) m r2 #->
-  Stream (Of c) m r3 #->
-  Stream (Of d) m (r1,r2,r3)
-zipWith3 = loop
-  where
-  loop :: Control.Monad m =>
-    (a -> b -> c -> d) ->
-    Stream (Of a) m r1 #->
-    Stream (Of b) m r2 #->
-    Stream (Of c) m r3 #->
-    Stream (Of d) m (r1,r2,r3)
-  loop f s1 s2 s3 = s1 & \case
-    Effect ms -> Effect $ ms >>= \s -> return $ zipWith3 f s s2 s3
-    Return r1 -> Effect $ do
-      r2 <- effects s2
-      r3 <- effects s3
-      return $ Return (r1,r2,r3)
-    Step (a :> as) -> s2 & \case
-      Effect ms -> Effect $
-        ms >>= \s -> return $ zipWith3 f (Step (a :> as)) s s3
-      Return r2 -> Effect $ do
-        r1 <- effects as
-        r3 <- effects s3
-        return $ Return (r1,r2,r3)
-      Step (b :> bs) -> s3 & \case
-        Effect ms -> Effect $
-          ms >>= \s -> return $ zipWith3 f (Step (a :> as)) (Step (b :> bs)) s
-        Return r3 -> Effect $ do
-          r1 <- effects as
-          r2 <- effects bs
-          return $ Return (r1,r2,r3)
-        Step (c :> cs) -> Step $ (f a b c) :> zipWith3 f as bs cs
-    where
-      Builder{..} = monadBuilder
-
--- | Like 'zip' but with three streams. Note: the remainders of the
--- two longer streams are exhausted.
-zip3 :: Control.Monad m =>
-  Stream (Of a) m r1 #->
-  Stream (Of b) m r2 #->
-  Stream (Of c) m r3 #->
-  Stream (Of (a,b,c)) m (r1,r2,r3)
-zip3 = zipWith3 (,,)
 
 unzip :: Control.Monad m =>
   Stream (Of (a, b)) m r #-> Stream (Of a) (Stream (Of b) m) r
