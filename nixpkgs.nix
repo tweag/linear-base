1755b155b225f25311e3e3afb35e1c53113b5e37
Utku Demir <me@utdemir.com>
2020-08-31 11:11:50 +1200

    Update nixpkgs

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,7 +1,7 @@
 let
-  # 2020-08-22 master
-  rev = "d8e671676d91fa9d3ef64b69ba7680d07d79f7a7";
-  sha256 = "0bpf3qncb1qxg7dx4rgmwbhbi74lia6jdzjvmg8wvalc738p6wgw";
+  # 2020-08-31 master
+  rev = "6716867eb3e763818000eab04f378b86cadc2894";
+  sha256 = "19fnqwpq1rk4iibbgq04j9w72s4n31nlz7m26iqhqd5lh7p8sc42";
 in
 import (fetchTarball {
   inherit sha256;
