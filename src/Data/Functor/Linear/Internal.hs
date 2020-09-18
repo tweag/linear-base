96377a071ce19e690d13617f00b7d6e8fdce53d6
Arnaud Spiwack <arnaud@spiwack.net>
2020-09-18 14:58:03 +0200

    Add setSwap combinator
    With a linear lens, we cannot linearly set the value at the lens site
    in general, however, we can swap the value at the lens site for
    another, which we get out of the operation.
    
    Ported from #79.

--- src/Data/Functor/Linear/Internal.hs
+++ src/Data/Functor/Linear/Internal.hs
@@ -5,6 +5,7 @@ module Data.Functor.Linear.Internal where
 
 import Prelude.Linear.Internal
 import Prelude (Maybe(..), Either(..))
+import Data.Functor.Compose
 import Data.Functor.Const
 import Data.Monoid.Linear
 import Data.Functor.Identity
@@ -96,6 +97,14 @@ instance Functor Identity where
 instance Applicative Identity where
   pure = Identity
   Identity f <*> Identity x = Identity (f x)
+
+instance (Functor f, Functor g) => Functor (Compose f g) where
+  fmap f (Compose x) = Compose (fmap (fmap f) x)
+
+instance (Applicative f, Applicative g) => Applicative (Compose f g) where
+   pure x = Compose (pure (pure x))
+   (Compose f) <*> (Compose x) = Compose (liftA2 (<*>) f x)
+   liftA2 f (Compose x) (Compose y) = Compose (liftA2 (liftA2 f) x y)
 
 ---------------------------------
 -- Monad transformer instances --
