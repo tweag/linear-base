6804ba470e132bae66de01dece001a5d41da2769
Utku Demir <me@utdemir.com>
2020-09-26 11:15:19 +1200

    Implement some Eq instances

--- src/Data/Eq/Linear.hs
+++ src/Data/Eq/Linear.hs
@@ -1,7 +1,5 @@
-{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE LinearTypes #-}
 {-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE StandaloneDeriving #-}
 
@@ -34,9 +32,46 @@ class Eq a where
   x /= y = not (x == y)
   infix 4 ==, /=
 
+-- * Instances
+
+instance Prelude.Eq a => Eq (Ur a) where
+  Ur x == Ur y = x Prelude.== y
+  Ur x /= Ur y = x Prelude./= y
+
+instance (Consumable a, Eq a) => Eq [a] where
+  [] == [] = True
+  (x:xs) == (y:ys) = x == y && xs == ys
+  xs == ys = (xs, ys) `lseq` False
+
+instance (Consumable a, Eq a) => Eq (Prelude.Maybe a) where
+  Prelude.Nothing == Prelude.Nothing = True
+  Prelude.Just x == Prelude.Just y = x == y
+  x == y = (x, y) `lseq` False
+
+instance (Consumable a, Consumable b, Eq a, Eq b)
+  => Eq (Prelude.Either a b) where
+  Prelude.Left x == Prelude.Left y = x == y
+  Prelude.Right x == Prelude.Right y = x == y
+  x == y = (x, y) `lseq` False
+
+instance (Eq a, Eq b) => Eq (a, b) where
+  (a, b) == (a', b') =
+    a == a' && b == b'
+
+instance (Eq a, Eq b, Eq c) => Eq (a, b, c) where
+  (a, b, c) == (a', b', c') =
+    a == a' && b == b' && c == c'
+
+instance (Eq a, Eq b, Eq c, Eq d) => Eq (a, b, c, d) where
+  (a, b, c, d) == (a', b', c', d') =
+    a == a' && b == b' && c == c' && d == d'
+
 deriving via MovableEq () instance Eq ()
 deriving via MovableEq Prelude.Int instance Eq Prelude.Int
 deriving via MovableEq Prelude.Double instance Eq Prelude.Double
+deriving via MovableEq Prelude.Bool instance Eq Prelude.Bool
+deriving via MovableEq Prelude.Char instance Eq Prelude.Char
+deriving via MovableEq Prelude.Ordering instance Eq Prelude.Ordering
 
 newtype MovableEq a = MovableEq a
 
