0bed708a0d9a22a3d29faa0207ae1626bc9287a8
Utku Demir <me@utdemir.com>
2021-05-13 12:01:44 +1200

    Update dependencies and remove overrides (#325)
    * Update dependencies and remove overrides
    
    * We don't need allow-newer too
    
    * Update nixpkgs

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,6 +1,6 @@
 let
-  rev = "f9b3e78132eabde9d01c27b82c6102c87a2ea5a1";
-  sha256 = "00dkynb7p9vy511h9g8b6a4r8j26iwsrn0s3433nhcr4v93q17z1";
+  rev = "acd49fab8ece11a89ef8ee49903e1cd7bb50f4b7";
+  sha256 = "1zbwifjak4ni71by4xdfp41byz152ll689hqy12pkdwnlspz3g8i";
 in
 import (fetchTarball {
   inherit sha256;
