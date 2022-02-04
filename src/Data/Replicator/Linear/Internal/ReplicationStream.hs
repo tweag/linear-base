aa5e6da65ddfa37d8f1e6daf228d1cfc538505a9
Facundo Domínguez <facundo.dominguez@tweag.io>
2022-02-04 10:55:49 -0300

    Fix reference to dup2

--- src/Data/Replicator/Linear/Internal/ReplicationStream.hs
+++ src/Data/Replicator/Linear/Internal/ReplicationStream.hs
@@ -22,7 +22,7 @@ import Prelude.Linear.Internal
 -- necessary, and @c@ is used to consume @s@ when consuming the stream.
 --
 -- Although it isn't enforced at type level, @dup2@ should abide by the same
--- laws as 'Data.Unrestricted.Dupable.dup2':
+-- laws as 'Data.Unrestricted.Linear.dup2':
 -- * @first c (dup2 a) ≃ a ≃ second c (dup2 a)@ (neutrality)
 -- * @first dup2 (dup2 a) ≃ (second dup2 (dup2 a))@ (associativity)
 --
