ad7b1221caa5fa32f19358f7047c91d44b7dc310
Arnaud Spiwack <arnaud@spiwack.net>
2022-02-17 11:45:14 +0100

    A more robust implementation of Replicator.elim
    I rely on more type families (I resurrected the `FunN` type family,
    for instance) and additional type classes in order to give robust
    hints to the type checker to infer some of the arguments based on the
    others in multiple possible ways.
    
    The previous version was abusing functional dependencies to that
    effect. The problem is that it almost certainly depended on bugs. Plus
    some behaviour changed in 9.2 making the recursive instance not
    typecheck anymore.
    
    The actual (staging) recursion now uses (type-level) Peano integers,
    this way we can get rid of the `OVERLAPPABLE` instances altogether,
    and avoid the sort of fragile behaviour that caused the failure in
    9.2.
    
    The 'elim' function itself has more type arguments, so I took it out
    of the type class. A collection of type families and type classes fill
    the role that functional dependencies used to play: figuring out `f`
    from `n` or `n` from `f`.
    
    Fixes #388.

--- src/Data/Arity/Linear/Internal.hs
+++ src/Data/Arity/Linear/Internal.hs
@@ -1,13 +1,56 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE LinearTypes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
 
-module Data.Arity.Linear.Internal (Arity) where
+module Data.Arity.Linear.Internal where
 
+import Data.Kind
 import GHC.TypeLits
+import GHC.Types
 
+data Peano = Z | S Peano
+
+type NatToPeano :: Nat -> Peano
+type family NatToPeano n where
+  NatToPeano 0 = 'Z
+  NatToPeano n = 'S (NatToPeano (n - 1))
+
+type PeanoToNat :: Peano -> Nat
+type family PeanoToNat n where
+  PeanoToNat 'Z = 0
+  PeanoToNat ('S n) = 1 + PeanoToNat n
+
+type FunN :: Peano -> Type -> Type -> Type
+type family FunN n a b where
+  FunN 'Z _ b = b
+  FunN ('S n) a b = a %1 -> FunN n a b
+
+-- | The 'Arity' type family exists to help the type checker fill in
+-- blanks. Chances are that you can safely ignore 'Arity' completely if it's in
+-- the type of a function you care. But read on if you are curious.
+--
+-- The idea is that in a function like 'Data.Replicator.Linear.elim' some of the
+-- type arguments are redundant. The function has an ambiguous type, so you will
+-- always have to help the compiler either with a type annotation or a type
+-- application. But there are several complete ways to do so. In
+-- 'Data.Replicator.Linear.elim', if you give the values of `n`, `a`, and `b`,
+-- then you can deduce the value of `f` (indeed, it's @'FunN' n a b@). With
+-- 'Arity' we can go in the other direction: if `b` and `f` are both known, then
+-- we know that `n` is @'Arity' b f@
+--
+-- 'Arity' returns a 'Nat' rather than a 'Peano' because the result is never
+-- consumed. It exists to infer arguments to functions such as
+-- 'Data.Replicator.Linear.elim' from the other arguments if they are known.
+--
+-- 'Arity' could be an associated type family to the 'IsFunN' type class. But
+-- it's better to make it a closed type family (which can't be associated to a
+-- type class) because it lets us give a well-defined error case.
+type Arity :: Type -> Type -> Nat
 type family Arity b f where
   Arity b b = 0
   Arity b (a %1 -> f) = Arity b f + 1
@@ -19,3 +62,26 @@ type family Arity b f where
           ':<>: 'ShowType b
           ':<>: 'Text "."
       )
+
+-- | The 'IsFun' type class is meant to help the type checker fill in
+-- blanks. Chances are that you can safely ignore 'IsFun' completely if it's in
+-- the type of a function you care. But read on if you are curious.
+--
+-- The type class 'IsFun' is a kind of inverse to 'FunN', it is meant to be
+-- read as @'IsFunN' a b f@ if and only if there exists `n` such that @f =
+-- 'FunN' n a b@ (`n` can be retrieved as @'Arity' b f@).
+--
+-- The reason why 'Arity' (read its documentation first) is not sufficient for
+-- our purpose, is that it can find `n` /if/ `f` is a linear function of the
+-- appropriate shape. But what if `f` is partially undetermined? Then it is
+-- likely that 'Arity' will be stuck. But we know, for instance, that if `f = a1
+-- %1 -> a2 %1 -> c` then we must have `a1 ~ a2`. The trick is that instance
+-- resolution of 'IsFun' will add unification constraints that the type checker
+-- has to solve. Look in particular at the instance @'IsFunN' a b (a' %p ->
+-- f))@: it matches liberally, so triggers on quite underdetermined `f`, but has
+-- equality constraints in its context which will help the type checker.
+class IsFunN a b f
+
+instance IsFunN a b b
+
+instance (IsFunN a b f, a' ~ a, p ~ 'One) => IsFunN a b (a' %p -> f)
