6faf3e37db538029251b27a7e0fb560e8f193fce
David Feuer <David.Feuer@gmail.com>
2022-02-21 23:41:28 -0500

    Consumable

--- src/Data/Unrestricted/Linear/Internal/Consumable.hs
+++ src/Data/Unrestricted/Linear/Internal/Consumable.hs
@@ -1,4 +1,17 @@
+{-# OPTIONS_GHC -Wno-partial-type-signatures #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE EmptyCase #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE LinearTypes #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE Trustworthy #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_HADDOCK hide #-}
 
 module Data.Unrestricted.Linear.Internal.Consumable
@@ -6,8 +19,23 @@ module Data.Unrestricted.Linear.Internal.Consumable
     Consumable (..),
     lseq,
     seqUnit,
-  )
-where
+    -- * Generic deriving
+    GConsumable,
+    genericConsume
+  ) where
+
+import Prelude.Linear.Generically
+import Prelude.Linear.Internal
+import qualified Unsafe.Linear as Unsafe
+import Generics.Linear
+import GHC.Types (Multiplicity (..))
+import qualified Prelude as Prelude
+import Data.List.NonEmpty (NonEmpty)
+import Data.Unrestricted.Linear.Internal.Ur
+import qualified Data.Semigroup as Semigroup
+import qualified Data.Monoid as Monoid
+import GHC.Tuple (Solo)
+import qualified Data.Vector as Vector
 
 class Consumable a where
   consume :: a %1 -> ()
@@ -24,3 +52,144 @@ lseq :: Consumable a => a %1 -> b %1 -> b
 lseq a b = seqUnit (consume a) b
 
 infixr 0 `lseq` -- same fixity as base.seq
+
+-- ----------------
+-- Instances
+
+instance Consumable a => Consumable (Vector.Vector a) where
+  consume xs = consume (Unsafe.toLinear Vector.toList xs)
+
+-- Prelude and primitive instances
+
+deriving via Generically Prelude.Char
+  instance Consumable Prelude.Char
+deriving via Generically Prelude.Double
+  instance Consumable Prelude.Double
+deriving via Generically Prelude.Float
+  instance Consumable Prelude.Float
+deriving via Generically Prelude.Int
+  instance Consumable Prelude.Int
+deriving via Generically Prelude.Word
+  instance Consumable Prelude.Word
+deriving via Generically Prelude.Ordering
+  instance Consumable Prelude.Ordering
+deriving via Generically Prelude.Bool
+  instance Consumable Prelude.Bool
+
+deriving via Generically ()
+  instance Consumable ()
+deriving via Generically (Solo a)
+  instance _ => Consumable (Solo a)
+deriving via Generically (a, b)
+  instance _ => Consumable (a, b)
+deriving via Generically (a, b, c)
+  instance _ => Consumable (a, b, c)
+deriving via Generically (a, b, c, d)
+  instance _ => Consumable (a, b, c, d)
+deriving via Generically (a, b, c, d, e)
+  instance _ => Consumable (a, b, c, d, e)
+deriving via Generically (a, b, c, d, e, f)
+  instance _ => Consumable (a, b, c, d, e, f)
+
+deriving via Generically (Prelude.Maybe a)
+  instance _ => Consumable (Prelude.Maybe a)
+deriving via Generically (Prelude.Either e a)
+  instance _ => Consumable (Prelude.Either e a)
+deriving via Generically [a]
+  instance _ => Consumable [a]
+deriving via Generically (NonEmpty a)
+  instance _ => Consumable (NonEmpty a)
+
+deriving via Generically (Ur a)
+  instance Consumable (Ur a)
+
+-- Data.Semigroup instances
+
+deriving via Generically (Semigroup.Arg a b)
+  instance _ => Consumable (Semigroup.Arg a b)
+deriving newtype instance _ => Consumable (Semigroup.Min a)
+deriving newtype instance _ => Consumable (Semigroup.Max a)
+deriving newtype instance _ => Consumable (Semigroup.First a)
+deriving newtype instance _ => Consumable (Semigroup.Last a)
+deriving newtype instance _ => Consumable (Semigroup.WrappedMonoid a)
+deriving newtype instance _ => Consumable (Semigroup.Dual a)
+deriving newtype instance Consumable Semigroup.All
+deriving newtype instance Consumable Semigroup.Any
+deriving newtype instance _ => Consumable (Semigroup.Sum a)
+deriving newtype instance _ => Consumable (Semigroup.Product a)
+
+-- Data.Monoid instances
+
+deriving newtype instance _ => Consumable (Monoid.First a)
+deriving newtype instance _ => Consumable (Monoid.Last a)
+deriving newtype instance _ => Consumable (Monoid.Alt f a)
+deriving newtype instance _ => Consumable (Monoid.Ap f a)
+
+-- ----------------
+-- Generic deriving
+
+instance (Generic a, GConsumable (Rep a)) => Consumable (Generically a) where
+  consume (Generically x) = genericConsume x
+
+genericConsume :: (Generic a, GConsumable (Rep a)) => a %1-> ()
+genericConsume = gconsume . from
+{-# INLINABLE genericConsume #-}
+
+-- | A class for generic representations that can be consumed.
+class GConsumable f where
+  gconsume :: f p %1-> ()
+instance GConsumable V1 where
+  gconsume = \case
+  {-# INLINE gconsume #-}
+instance GConsumable U1 where
+  gconsume U1 = ()
+  {-# INLINE gconsume #-}
+instance (GConsumable f, GConsumable g) => GConsumable (f :+: g) where
+  gconsume (L1 a) = gconsume a
+  gconsume (R1 a) = gconsume a
+  {-# INLINE gconsume #-}
+instance (GConsumable f, GConsumable g) => GConsumable (f :*: g) where
+  gconsume (a :*: b) = gconsume a `seqUnit` gconsume b
+  {-# INLINE gconsume #-}
+instance Consumable c => GConsumable (K1 i c) where
+  gconsume (K1 c) = consume c
+  {-# INLINE gconsume #-}
+instance GConsumable f => GConsumable (M1 i t f) where
+  gconsume (M1 a) = gconsume a
+  {-# INLINE gconsume #-}
+
+-- This split is a bit awkward. We'd like to be able to *default*
+-- the multiplicity to `Many` when it's polymorphic. We'll be able
+-- to do that once the Exportable Named Defaults Proposal
+-- (https://github.com/ghc-proposals/ghc-proposals/pull/409#issuecomment-931839874)
+-- has been implemented. The same goes for Dupable and Movable.
+instance GConsumable (MP1 'Many f) where
+  gconsume (MP1 _) = ()
+  {-# INLINE gconsume #-}
+instance GConsumable f => GConsumable (MP1 'One f) where
+  gconsume (MP1 x) = gconsume x
+  {-# INLINE gconsume #-}
+
+-- Instances for unlifted generic representations
+--
+-- /!\ 'Char#', 'Double#', 'Float#', 'Int#', 'Word#' are unboxed data-types,
+-- and therefore they cannot have any linear values hidden in a closure
+-- anywhere. Therefore it is safe to call non-linear functions linearly on
+-- these types. We refrain from including a 'GConsumable' instance for 'UAddr'
+-- for the moment, as that seems potentially confusing—pointers usually
+-- must be created, duplicated, and destroyed rather carefully. /!\
+
+instance GConsumable UChar where
+  gconsume (UChar x) = Unsafe.toLinear (\_ -> ()) x
+
+instance GConsumable UDouble where
+  gconsume (UDouble x) = Unsafe.toLinear (\_ -> ()) x
+
+instance GConsumable UFloat where
+  gconsume (UFloat x) = Unsafe.toLinear (\_ -> ()) x
+
+instance GConsumable UInt where
+  gconsume (UInt x) = Unsafe.toLinear (\_ -> ()) x
+
+instance GConsumable UWord where
+  gconsume (UWord x) = Unsafe.toLinear (\_ -> ()) x
--- src/Data/Unrestricted/Linear/Internal/Instances.hs
+++ src/Data/Unrestricted/Linear/Internal/Instances.hs
@@ -64,22 +64,16 @@ instance Movable a => Dupable (AsMovable a) where
     move x & \case
       Ur x' -> Data.pure x'
 
-deriving via (AsMovable ()) instance Consumable ()
-
 deriving via (AsMovable ()) instance Dupable ()
 
 instance Movable () where
   move () = Ur ()
-
-deriving via (AsMovable Bool) instance Consumable Bool
 
 deriving via (AsMovable Bool) instance Dupable Bool
 
 instance Movable Bool where
   move True = Ur True
   move False = Ur False
-
-deriving via (AsMovable Int) instance Consumable Int
 
 deriving via (AsMovable Int) instance Dupable Int
 
@@ -134,8 +128,6 @@ instance Movable Int64 where
   -- copying an 'Int64#' and using it several times. /!\
   move (I64# i) = Unsafe.toLinear (\j -> Ur (I64# j)) i
 
-deriving via (AsMovable Double) instance Consumable Double
-
 deriving via (AsMovable Double) instance Dupable Double
 
 instance Movable Double where
@@ -144,8 +136,6 @@ instance Movable Double where
   -- non-linear functions linearly on this type: there is no difference between
   -- copying an 'Double#' and using it several times. /!\
   move (D# i) = Unsafe.toLinear (\j -> Ur (D# j)) i
-
-deriving via (AsMovable Word) instance Consumable Word
 
 deriving via (AsMovable Word) instance Dupable Word
 
@@ -200,14 +190,10 @@ instance Movable Word64 where
   -- copying an 'Word64#' and using it several times. /!\
   move (W64# i) = Unsafe.toLinear (\j -> Ur (W64# j)) i
 
-deriving via (AsMovable Char) instance Consumable Char
-
 deriving via (AsMovable Char) instance Dupable Char
 
 instance Movable Char where
   move (C# c) = Unsafe.toLinear (\x -> Ur (C# x)) c
-
-deriving via (AsMovable Ordering) instance Consumable Ordering
 
 deriving via (AsMovable Ordering) instance Dupable Ordering
 
@@ -219,17 +205,11 @@ instance Movable Ordering where
 -- TODO: instances for longer primitive tuples
 -- TODO: default instances based on the Generic framework
 
-instance (Consumable a, Consumable b) => Consumable (a, b) where
-  consume (a, b) = consume a `lseq` consume b
-
 instance (Dupable a, Dupable b) => Dupable (a, b) where
   dupR (a, b) = (,) Data.<$> dupR a Data.<*> dupR b
 
 instance (Movable a, Movable b) => Movable (a, b) where
   move (a, b) = (,) Data.<$> move a Data.<*> move b
-
-instance (Consumable a, Consumable b, Consumable c) => Consumable (a, b, c) where
-  consume (a, b, c) = consume a `lseq` consume b `lseq` consume c
 
 instance (Dupable a, Dupable b, Dupable c) => Dupable (a, b, c) where
   dupR (a, b, c) = (,,) Data.<$> dupR a Data.<*> dupR b Data.<*> dupR c
@@ -248,10 +228,6 @@ instance (KnownNat n, Dupable a) => Dupable (V n a) where
     V . Unsafe.toLinear (Vector.fromListN (V.theLength @n))
       Data.<$> dupR (Unsafe.toLinear Vector.toList xs)
 
-instance Consumable a => Consumable (Prelude.Maybe a) where
-  consume Prelude.Nothing = ()
-  consume (Prelude.Just x) = consume x
-
 instance Dupable a => Dupable (Prelude.Maybe a) where
   dupR Prelude.Nothing = Data.pure Prelude.Nothing
   dupR (Prelude.Just x) = Data.fmap Prelude.Just (dupR x)
@@ -259,10 +235,6 @@ instance Dupable a => Dupable (Prelude.Maybe a) where
 instance Movable a => Movable (Prelude.Maybe a) where
   move (Prelude.Nothing) = Ur Prelude.Nothing
   move (Prelude.Just x) = Data.fmap Prelude.Just (move x)
-
-instance (Consumable a, Consumable b) => Consumable (Prelude.Either a b) where
-  consume (Prelude.Left a) = consume a
-  consume (Prelude.Right b) = consume b
 
 instance (Dupable a, Dupable b) => Dupable (Prelude.Either a b) where
   dupR (Prelude.Left a) = Data.fmap Prelude.Left (dupR a)
@@ -272,10 +244,6 @@ instance (Movable a, Movable b) => Movable (Prelude.Either a b) where
   move (Prelude.Left a) = Data.fmap Prelude.Left (move a)
   move (Prelude.Right b) = Data.fmap Prelude.Right (move b)
 
-instance Consumable a => Consumable [a] where
-  consume [] = ()
-  consume (a : l) = consume a `lseq` consume l
-
 instance Dupable a => Dupable [a] where
   dupR [] = Data.pure []
   dupR (a : l) = (:) Data.<$> dupR a Data.<*> dupR l
@@ -284,16 +252,11 @@ instance Movable a => Movable [a] where
   move [] = Ur []
   move (a : l) = (:) Data.<$> move a Data.<*> move l
 
-instance Consumable a => Consumable (NonEmpty a) where
-  consume (x :| xs) = consume x `lseq` consume xs
-
 instance Dupable a => Dupable (NonEmpty a) where
   dupR (x :| xs) = (:|) Data.<$> dupR x Data.<*> dupR xs
 
 instance Movable a => Movable (NonEmpty a) where
   move (x :| xs) = (:|) Data.<$> move x Data.<*> move xs
-
-deriving via (AsMovable (Ur a)) instance Consumable (Ur a)
 
 deriving via (AsMovable (Ur a)) instance Dupable (Ur a)
 
@@ -301,25 +264,17 @@ instance Movable (Ur a) where
   move (Ur a) = Ur (Ur a)
 
 -- Some stock instances
-deriving instance Consumable a => Consumable (Sum a)
-
 deriving instance Dupable a => Dupable (Sum a)
 
 deriving instance Movable a => Movable (Sum a)
-
-deriving instance Consumable a => Consumable (Product a)
 
 deriving instance Dupable a => Dupable (Product a)
 
 deriving instance Movable a => Movable (Product a)
 
-deriving instance Consumable All
-
 deriving instance Dupable All
 
 deriving instance Movable All
-
-deriving instance Consumable Any
 
 deriving instance Dupable Any
 
