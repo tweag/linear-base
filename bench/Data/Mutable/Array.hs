4c1a859d7d7bfe8fcbbf23c28a2740a4a65c0201
Arnaud Spiwack <arnaud.spiwack@tweag.io>
2023-05-03 19:05:12 +0200

    Array benchmarks: use same implementation for both data structures
    I simply introduced a type class to abstract over the differences. I
    almost didn't do it because I was afraid that the extra `Ur` and `(,)`
    would pollute the result and bias the result in our favours. Maybe it
    does. The reads are a bit less different in this style. I guess it was
    to be expected because reads are so fast. But the benchmark still says
    that our implementation is slow. So I say: good enough.

--- bench/Data/Mutable/Array.hs
+++ bench/Data/Mutable/Array.hs
@@ -1,17 +1,24 @@
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE LinearTypes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NumericUnderscores #-}
-{-# LANGUAGE TypeApplications #-}
-
+{-# LANGUAGE QuantifiedConstraints #-}
 -- Uncomment the line below to observe the generated (optimised) Core. It will
 -- land in a file named “Array.dump-simpl”
 -- {-# OPTIONS_GHC -ddump-simpl -ddump-to-file -dsuppress-all -dsuppress-uniques #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Data.Mutable.Array (benchmarks) where
 
 import Control.DeepSeq (rnf)
 import qualified Data.Array.Mutable.Linear as Array.Linear
-import Data.Function ((&))
+import qualified Data.Array.Mutable.Linear as Array.Linear.Array
+import Data.Functor.Compose
+import Data.Kind
 import qualified Data.Unrestricted.Linear as Linear
 import qualified Data.Vector
 import qualified Prelude.Linear as Linear
@@ -43,12 +50,12 @@ data Impls
       (Data.Vector.Vector Int -> ())
 
 runImpls :: String -> Impls -> Int -> Benchmark
-runImpls name impls size =
+runImpls name impls sz0 =
   let Impls linear dataVector = impls
    in bgroup
         name
-        [ bench "Data.Array.Mutable.Linear" $ whnf (runLinear linear) size,
-          bench "Data.Vector" $ whnf (runDataVector dataVector) size
+        [ bench "Data.Array.Mutable.Linear" $ whnf (runLinear linear) sz0,
+          bench "Data.Vector" $ whnf (runDataVector dataVector) sz0
         ]
   where
     runLinear :: (Array.Linear.Array Int %1 -> ()) -> Int -> ()
@@ -57,108 +64,130 @@ runImpls name impls size =
     runDataVector :: (Data.Vector.Vector Int -> ()) -> Int -> ()
     runDataVector cb sz = cb (Data.Vector.replicate sz 0)
 
+type ArrayThing :: (Type -> Type) -> Constraint
+class ArrayThing arr where
+  size :: arr a %1 -> (Linear.Ur Int, arr a)
+  get :: Int -> arr a %1 -> (Linear.Ur a, arr a)
+  set :: Int -> a -> arr a %1 -> arr a
+  toList :: arr a %1 -> Linear.Ur [a]
+  amap :: (a -> b) -> arr a %1 -> arr b
+
+  -- | Note: I [Arnaud Spiwack] initially thought I could use
+  -- 'Consumable'/'consume' for this. But it doesn't work because the natural
+  -- 'consume' function for `Ur x` doesn't evaluate the `x` at all. We need to
+  -- evaluate the `x` in the 'Vector' instance.
+  force :: arr a %1 -> ()
+
+instance ArrayThing Array.Linear.Array where
+  size = Array.Linear.Array.size
+  get = Array.Linear.Array.unsafeGet
+  set = Array.Linear.Array.unsafeSet
+  toList = Array.Linear.Array.toList
+  amap = Array.Linear.Array.map
+  force = Linear.consume
+
+instance ArrayThing (Compose Linear.Ur Data.Vector.Vector) where
+  size (Compose (Linear.Ur v)) = (Linear.Ur (Data.Vector.length v), Compose (Linear.Ur v))
+  get i (Compose (Linear.Ur v)) = (Linear.Ur (v Data.Vector.! i), Compose (Linear.Ur v))
+  set i a (Compose (Linear.Ur v)) = Compose (Linear.Ur (v Data.Vector.// [(i, a)]))
+  toList (Compose (Linear.Ur v)) = Linear.Ur (Data.Vector.toList v)
+  amap f (Compose (Linear.Ur v)) = Compose (Linear.Ur (Data.Vector.map f v))
+  force (Compose (Linear.Ur v)) = v `seq` ()
+
+cleanup :: ((Compose Linear.Ur f a) %1 -> b) -> (f a -> b)
+cleanup k a = k (Compose (Linear.Ur a))
+
 --------------------------------------------------------------------------------
 
 bToList :: Impls
 bToList = Impls linear dataVector
   where
-    linear :: Array.Linear.Array Int %1 -> ()
-    linear hm =
-      hm
-        Linear.& Array.Linear.toList
+    impl :: (ArrayThing arr) => arr Int %1 -> ()
+    impl arr =
+      arr
+        Linear.& toList
         Linear.& Linear.lift rnf
         Linear.& Linear.unur
 
+    linear :: Array.Linear.Array Int %1 -> ()
+    linear = impl
+
     dataVector :: Data.Vector.Vector Int -> ()
-    dataVector hm =
-      hm
-        & Data.Vector.toList
-        & rnf
+    dataVector = cleanup impl
 {-# NOINLINE bToList #-}
 
 bMap :: Impls
 bMap = Impls linear dataVector
   where
+    impl :: (ArrayThing arr) => arr Int %1 -> ()
+    impl arr =
+      arr
+        Linear.& amap (+ 1)
+        Linear.& get 5
+        Linear.& \case
+          (Linear.Ur _, arr') -> force arr'
+
     linear :: Array.Linear.Array Int %1 -> ()
-    linear hm =
-      hm
-        Linear.& Array.Linear.map (+ 1)
-        Linear.& Array.Linear.unsafeGet 5
-        Linear.& (`Linear.lseq` ())
+    linear = impl
 
     dataVector :: Data.Vector.Vector Int -> ()
-    dataVector hm =
-      hm
-        & Data.Vector.map (+ 1)
-        & dontFuse -- This looks like cheating, I know. But we're trying to measure
-        -- the speed of `map`, and without this, `vector` fuses the `map`
-        -- with the subsequent `index` to skip writing to the rest of the
-        -- vector.
-        & (`Data.Vector.unsafeIndex` 5)
-        & (`seq` ())
+    dataVector = cleanup impl
 {-# NOINLINE bMap #-}
 
 bReads :: Impls
 bReads = Impls linear dataVector
   where
-    linear :: Array.Linear.Array Int %1 -> ()
-    linear hm =
-      hm
-        Linear.& Array.Linear.size
+    impl :: (ArrayThing arr) => arr Int %1 -> ()
+    impl arr0 =
+      arr0
+        Linear.& size
         Linear.& \(Linear.Ur sz, arr) ->
           arr
             Linear.& go 0 sz
       where
-        go :: Int -> Int -> Array.Linear.Array Int %1 -> ()
+        go :: (ArrayThing arr) => Int -> Int -> arr Int %1 -> ()
         go start end arr
           | start < end =
-              Array.Linear.unsafeGet start arr
+              get start arr
                 Linear.& \(Linear.Ur i, arr') -> i `Linear.seq` go (start + 1) end arr'
-          | otherwise = arr `Linear.lseq` ()
+          | otherwise = force arr
+
+    linear :: Array.Linear.Array Int %1 -> ()
+    linear = impl
 
     dataVector :: Data.Vector.Vector Int -> ()
-    dataVector v =
-      let sz = Data.Vector.length v
-       in go 0 sz
-      where
-        go :: Int -> Int -> ()
-        go start end
-          | start < end =
-              (v Data.Vector.! start) `seq` go (start + 1) end
-          | otherwise = ()
+    dataVector = cleanup impl
 {-# NOINLINE bReads #-}
 
 bAlloc :: Impls
 bAlloc = Impls linear dataVector
   where
+    impl :: (ArrayThing arr) => arr Int %1 -> ()
+    impl = force
+
     linear :: Array.Linear.Array Int %1 -> ()
-    linear = Linear.consume
+    linear = impl
 
     dataVector :: Data.Vector.Vector Int -> ()
-    dataVector = (`seq` ())
+    dataVector = cleanup impl
 {-# NOINLINE bAlloc #-}
 
 bSets :: Impls
 bSets = Impls linear dataVector
   where
-    linear :: Array.Linear.Array Int %1 -> ()
-    linear arr0 =
-      case Array.Linear.size arr0 of
+    impl :: (ArrayThing arr) => arr Int %1 -> ()
+    impl arr0 =
+      case size arr0 of
         (Linear.Ur sz, arr) -> go 0 sz arr
       where
-        go :: Int -> Int -> Array.Linear.Array Int %1 -> ()
+        go :: (ArrayThing arr) => Int -> Int -> arr Int %1 -> ()
         go start end arr
           | start < end =
-              go (start + 1) end Linear.$ Array.Linear.unsafeSet start 42 arr
-          | otherwise = arr `Linear.lseq` ()
+              go (start + 1) end Linear.$ set start 42 arr
+          | otherwise = force arr
+
+    linear :: Array.Linear.Array Int %1 -> ()
+    linear = impl
 
     dataVector :: Data.Vector.Vector Int -> ()
-    dataVector v0 =
-      let sz = Data.Vector.length v0
-       in go 0 sz v0
-      where
-        go :: Int -> Int -> Data.Vector.Vector Int -> ()
-        go start end v
-          | start < end =
-              go (start + 1) end $ v Data.Vector.// [(start, 42)]
-          | otherwise = v `seq` ()
+    dataVector = cleanup impl
