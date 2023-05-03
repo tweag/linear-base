81e1e9471d26a9d51185ec80fa76728dde7bce87
Thomas BAGREL <thomas.bagrel@tweag.io>
2023-05-03 09:44:54 +0200

    Moving from gauge to tasty-bench

--- bench/Main.hs
+++ bench/Main.hs
@@ -1,12 +1,12 @@
 module Main where
 
 import qualified Data.Mutable.Array as Array
-import Data.Mutable.HashMap (hmbench)
-import Gauge
+import qualified Data.Mutable.HashMap as HashMap
+import Test.Tasty.Bench (defaultMain)
 
 main :: IO ()
 main = do
   defaultMain
-    [ hmbench,
-      Array.benchmarks
+    [ Array.benchmarks,
+      HashMap.benchmarks
     ]
