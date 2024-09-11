5e85ae9277267acfa84cd9e8d13db5ce6baea437
Thomas BAGREL <thomas.bagrel@tweag.io>
2024-09-11 12:51:37 +0200

    Upgrade cabal-docspec

--- nix/cabal-docspec.nix
+++ nix/cabal-docspec.nix
@@ -4,8 +4,8 @@ pkgs.stdenv.mkDerivation {
   name = "cabal-docspec";
 
   src = pkgs.fetchurl {
-    url = "https://github.com/phadej/cabal-extras/releases/download/cabal-docspec-0.0.0.20230406/cabal-docspec-0.0.0.20230406-x86_64-linux.xz";
-    sha256 = "68fa9addd5dc453d533a74a763950499d4593b1297c9a05c3ea5bd1acc04c9dd";
+    url = "https://github.com/phadej/cabal-extras/releases/download/cabal-docspec-0.0.0.20240703/cabal-docspec-0.0.0.20240703-x86_64-linux.xz";
+    sha256 = "48bf3b7fd2f7f0caa6162afee57a755be8523e7f467b694900eb420f5f9a7b76";
   };
 
   phases = ["installPhase"];
